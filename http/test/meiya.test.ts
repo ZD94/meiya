25a600a276f217c1d1c49fd8f3ef3bfdc8e8bfb2
张栋 <dong.zhang@jingli365.com>
2017-11-27 10:25:46 +0800

    change

--- http/test/meiya.test.ts
+++ http/test/meiya.test.ts
@@ -58,7 +58,7 @@ describe('/美亚订票流程', function () {
             qs: {
                 "departureCode": "PEK",
                 "arrivalCode": "SHA",
-                "depDate": "2018-01-20",
+                "depDate": "2018-01-22",
                 supplier: "meiya",
                 tripType: 1
             }
@@ -73,117 +73,134 @@ describe('/美亚订票流程', function () {
             } catch (err) {
                 result = body
             }
-            console.log(result, "<========result");
-            // data = result.data[1];
-            // flightID = data["flightPriceInfoList"][0].flightID;
-            // departureCity = data.orgAirportCode;
-            // arrivalCity = data.desAirportCode;
-            // departureDate = data.depDate;
-            // airline = data.airline;
-            // cabinType = data.flightPriceInfoList[0].cabinType;
-            // flightNo = data.flightNo;
-            // price = Number(data.flightPriceInfoList[0].ticketPrice);
-            //
-            // data2 = result.data[2];
-            // flightID2 = data["flightPriceInfoList"][0].flightID;
-            // departureCity2 = data.orgAirportCode;
-            // arrivalCity2 = data.desAirportCode;
-            // departureDate2 = data.depDate;
-            // airline2 = data.airline;
-            // cabinType2 = data.flightPriceInfoList[0].cabinType;
-            // flightNo2 = data.flightNo;
-            // price2 = Number(data.flightPriceInfoList[0].ticketPrice);
+            console.log(result.data.data[1], "<========result");
+            data = result.data.data[1];
+            // console.log(data, "<========result");
+            flightID = data["flightPriceInfoList"][0].flightID;
+            departureCity = data.orgAirportCode;
+            arrivalCity = data.desAirportCode;
+            departureDate = data.depDate;
+            airline = data.airline;
+            cabinType = data.flightPriceInfoList[0].cabinType;
+            flightNo = data.flightNo;
+            price = Number(data.flightPriceInfoList[0].ticketPrice);
+
+            data2 = result.data[2];
+            flightID2 = data["flightPriceInfoList"][0].flightID;
+            departureCity2 = data.orgAirportCode;
+            arrivalCity2 = data.desAirportCode;
+            departureDate2 = data.depDate;
+            airline2 = data.airline;
+            cabinType2 = data.flightPriceInfoList[0].cabinType;
+            flightNo2 = data.flightNo;
+            price2 = Number(data.flightPriceInfoList[0].ticketPrice);
 
             expect(result.code).to.be.equal(0);
             done()
         })
     });
 
-    // let order;
-    // it("/创建订单", (done) => {
-    //     request({
-    //         url: url + "Order",
-    //         method: 'POST',
-    //         json: true,
-    //         headers: {
-    //             userName: "JingLiZhiXiang",
-    //             password: "123456"
-    //         },
-    //         body: {
-    //             "flightList":
-    //                 [{
-    //                     "flightID": `${flightID}`,
-    //                     "departureCode": `${departureCity}`,
-    //                     "arrivalCode": `${arrivalCity}`,
-    //                     "depDate": `${departureDate}`,
-    //                     "airline": `${airline}`,
-    //                     "cabinType": `${cabinType}`,
-    //                     "flightNo": `${flightNo}`,
-    //                     "price": price
-    //                 }],
-    //             "passengerList":
-    //                 [{
-    //                     "name": "张栋",
-    //                     "mobile": "15978561146",
-    //                     "passengerType": "1",
-    //                     "companyId": "S117325",
-    //                     "certificatesList": [{
-    //                         "certType": "身份证",
-    //                         "certNumber": "411527199408012773"
-    //                     }]
-    //                 }],
-    //             "contactList":
-    //                 {
-    //                     "name": "张栋",
-    //                     "mobile": "15978561146"
-    //                 },
-    //             "type": "order",
-    //             // "sessionId": sessionId
-    //         }
-    //     }, (err, res, body) => {
-    //         if (err) {
-    //             console.log(err);
-    //             return
-    //         }
-    //         let result;
-    //         try {
-    //             result = res.body
-    //         } catch (err) {
-    //             result = body
-    //         }
-    //         order = result.data.orderNos[0];
-    //         expect(result.code).to.be.equal(0);
-    //         done()
-    //     })
-    // });
-    //
-    // it("提交审批", (done) => {
-    //     request({
-    //         url: url + "Order/" + order,
-    //         method: "PUT",
-    //         headers: {
-    //             userName: "JingLiZhiXiang",
-    //             password: "123456"
-    //         },
-    //         json: true,
-    //         body: {
-    //             "type": "order",
-    //         }
-    //     }, (err, res, body) => {
-    //         if (err) {
-    //             console.log(err);
-    //             return
-    //         }
-    //         let result;
-    //         try {
-    //             result = res.body;
-    //         } catch (err) {
-    //             result = body
-    //         }
-    //         expect(result.code).to.be.equal(0);
-    //         done()
-    //     })
-    // });
+    let order;
+    it("/创建订单", (done) => {
+        let info = {
+            username: "JingLiZhiXiang",
+            password: "123456"
+        };
+
+        let str = JSON.stringify(info);
+        str = encodeURIComponent(str);
+        request({
+            url: url + "Order",
+            method: 'POST',
+            json: true,
+            headers: {
+                auth: str,
+                supplier: "meiya"
+            },
+            body: {
+                "flightList":
+                    [{
+                        "flightID": `${flightID}`,
+                        "departureCode": `${departureCity}`,
+                        "arrivalCode": `${arrivalCity}`,
+                        "depDate": `${departureDate}`,
+                        "airline": `${airline}`,
+                        "cabinType": `${cabinType}`,
+                        "flightNo": `${flightNo}`,
+                        "price": price
+                    }],
+                "passengerList":
+                    [{
+                        "name": "张栋",
+                        "mobile": "15978561146",
+                        "passengerType": "1",
+                        "companyId": "S117325",
+                        "certificatesList": [{
+                            "certType": "身份证",
+                            "certNumber": "411527199408012773"
+                        }]
+                    }],
+                "contactList":
+                    {
+                        "name": "张栋",
+                        "mobile": "15978561146"
+                    },
+                "type": "order",
+                // "sessionId": sessionId
+            }
+        }, (err, res, body) => {
+            if (err) {
+                console.log(err);
+                return
+            }
+            let result;
+            try {
+                result = res.body
+            } catch (err) {
+                result = body
+            }
+            order = result.data.orderNos[0];
+            expect(result.code).to.be.equal(0);
+            done()
+        })
+    });
+
+    it("提交审批", (done) => {
+
+        let info = {
+            username: "JingLiZhiXiang",
+            password: "123456"
+        };
+
+        let str = JSON.stringify(info);
+        str = encodeURIComponent(str);
+
+        request({
+            url: url + "Order/" + order,
+            method: "PUT",
+            headers: {
+                auth: str,
+                supplier: "meiya"
+            },
+            json: true,
+            body: {
+                "type": "order",
+            }
+        }, (err, res, body) => {
+            if (err) {
+                console.log(err);
+                return
+            }
+            let result;
+            try {
+                result = res.body;
+            } catch (err) {
+                result = body
+            }
+            expect(result.code).to.be.equal(0);
+            done()
+        })
+    });
     //
     // it("订单详情", (done) => {
     //     request({
