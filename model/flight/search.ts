d92d8196ca18a3a76396574c0b78f1953d9386d5
hexisen <616082046@qq.com>
2017-11-27 15:57:51 +0800

    test console

--- model/flight/search.ts
+++ model/flight/search.ts
@@ -23,6 +23,8 @@ export async function searchFlight(query): Promise<ReplyData> {
     ];
     let querys = transAttributeName(query, testArr);
 
+
+    console.log("searchFlight:==> ",querys)
     let params = {
         url: `${config.meiyaUrl}` + "/QueryFlights",
         body: querys,
