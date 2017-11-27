25e53dd3be45596f91793e0dd8fa01dbc961b61a
hexisen <616082046@qq.com>
2017-11-27 16:33:48 +0800

    add console

--- app.ts
+++ app.ts
@@ -11,11 +11,16 @@ var logger = new Logger('main');
 
 let app = express();
 app.use(conn_timeout("15s"));
-app.use(bodyParser.json({limit: '8mb'}));
-app.use(bodyParser.urlencoded({limit: '8mb', extended: true}));
+app.use(bodyParser.json({ limit: '8mb' }));
+app.use(bodyParser.urlencoded({ limit: '8mb', extended: true }));
 app.use(express.static(path.join(__dirname, "www")));
 
 import router from "./http";
+
+app.get("/test", (req, res, next) => {
+    console.log("test has been called.");
+    res.send("test ok");
+});
 
 app.use((req, res, next) => {
     logger.info(req.method, req.url, process.title);
@@ -23,13 +28,15 @@ app.use((req, res, next) => {
     // if (tmckey != config.tmckey) {
     //     return res.sendStatus(403);
     // }
+    console.log(req.headers);
+    console.log(req.query);
+    console.log(req.body);
+
+
     next();
 });
 app.use(router);
 
-app.get("/test", (req, res, next) => {
-    console.log("test has been called.");
-    res.send("test ok");
-});
+
 
 export default app;
