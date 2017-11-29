e3c8be370de7aef349022a6e2f6eb5fa8ce4b38d
李泽霖 <zelin.li@jingli365.com>
2017-11-29 16:50:55 +0800

    ooooh

--- model/notification/index.ts
+++ model/notification/index.ts
@@ -1,17 +1,23 @@
-let config = require('@jingli/config');
-let crypto = require('crypto');
-import {transAttributeName} from 'http/util';
+// let config = require('@jingli/config');
+// let crypto = require('crypto');
+// import {transAttributeName} from 'http/util';
+
+// let key = '';
 
 
-//处理美亚发送的预订通知
-export async function handleNotificationMY(query): Promise<{code: any}> {
-    let request: any = query.request;
-    let password: string = request.password;
-    let timeStamp: string = request.timeStamp;
-    let msgType: string = request.msgType;
-    let data: object = request.data;
+// //处理美亚发送的预订通知
+// export async function handleNotificationMY(query): Promise<{code: any}> {
+//     let request: any = query.request;
+//     let password: string = request.password;
+//     let timeStamp: string = request.timeStamp;
+//     let msgType: string = request.msgType;
+//     let data: object = request.data;
 
-    //对password进行SHA1解密,取出timeStamp和key进行比对校验
-    
+//     //将password和key进行字典序排序后SHA1加密,与得到的password进行比对校验    
+//     let str: string[] = [timeStamp.toLocaleLowerCase(), key.toLocaleLowerCase()];
 
-}
+//     //sort
+//     for (let i = 0; i < str.length; i++) {
+//         str.sort(function(a, b):number {return (a- b);});
+//     }
+// }
