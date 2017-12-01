67092d60bd755b50bb61212dbc09ec12cee7efda
李泽霖 <zelin.li@jingli365.com>
2017-12-01 17:31:56 +0800

    asdf

--- model/notification/index.ts
+++ model/notification/index.ts
@@ -3,22 +3,27 @@ let crypto = require('crypto');
 let fs = require('fs');
 import {transAttributeName} from 'http/util';
 
-let key = '';
+let key = '4f3f29eb05ee4cda81528647e91608d4';
 
 
 //处理美亚发送的预订通知
 export async function handleMYNotification(query, num): Promise<{code: string, description: string}> {
-    let request: any = query.request;
-    let password: string = request.password;
-    let timeStamp: string = request.timeStamp;
-    let msgType: string = request.msgType;
-    let data: object = request.data;
+    let password: string = query.password;
+    let timeStamp: string = query.timeStamp;
+    let msgType: string = query.msgType;
+    let data: object = query.data;
 
     //将password和key进行字典序排序后SHA1加密,与得到的password进行比对校验    
     let str: string[] = [timeStamp.toLocaleLowerCase(), key.toLocaleLowerCase()];
 
     //sort
     str.sort();
+
+    if (str[0].toLocaleUpperCase() == timeStamp.toLocaleLowerCase()) {
+        str = [timeStamp, key];
+    } else {
+        str = [key, timeStamp];
+    }
 
     //concat
     let testStr: string = '';
@@ -27,12 +32,13 @@ export async function handleMYNotification(query, num): Promise<{code: string, d
     }
 
     //cipher
+    console.log('testStr', testStr);
     let cipher = crypto.createHash('sha1');
     cipher.update(testStr);
-    let crypted = cipher.digest('base64');
+    let crypted = cipher.digest('hex');
     
     if (crypted == password) { //校验成功
-        fs.writeFileSync(`MYNotification/result_${num}.json`, JSON.stringify(data), 'utf-8'); 
+        fs.writeFileSync(`log/MYresult_${num}.json`, JSON.stringify(data), 'utf-8'); 
         return {code: '10000', description: '通知成功'};
     } else {
         console.log('crypted======>', crypted);
